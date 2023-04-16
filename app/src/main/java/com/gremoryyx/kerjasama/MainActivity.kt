38fc06fdff9d98b820c87153ecb7a8d57ecb1e04
gremoryyx <arispelgremory@gmail.com>
2023-04-16 16:05:05 +0800

    connect fragments with navbar

--- app/src/main/java/com/gremoryyx/kerjasama/MainActivity.kt
+++ app/src/main/java/com/gremoryyx/kerjasama/MainActivity.kt
@@ -2,10 +2,39 @@ package com.gremoryyx.kerjasama
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        replaceFragment(HomeFragment())
+
+        val bottomNavigationView = findViewById<com.google.android.material.bottomnavigation.BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.home -> {
+                    replaceFragment(HomeFragment())
+                }
+                R.id.profile -> {
+                    replaceFragment(ProfileFragment())
+                }
+                R.id.list -> {
+                    replaceFragment(ListFragment())
+                }
+                R.id.notification -> {
+                    replaceFragment(NotificationsFragment())
+                }
+            }
+            true
+        }
+
     }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
+    }
+
 }
