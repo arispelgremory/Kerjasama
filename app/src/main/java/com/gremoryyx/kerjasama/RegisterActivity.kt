1580aff70e826b7f8b2053e637d3d768da83de70
gremoryyx <arispelgremory@gmail.com>
2023-04-21 22:21:28 +0800

    Registration Step 1 done

--- app/src/main/java/com/gremoryyx/kerjasama/RegisterActivity.kt
+++ app/src/main/java/com/gremoryyx/kerjasama/RegisterActivity.kt
@@ -1,29 +1,67 @@
 package com.gremoryyx.kerjasama
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 
-class RegisterActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity(), RegisterBasicInfoFragment.OnFragmentInteractionListener {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
+
         if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.register_fragment_container, RegisterBasicInfoFragment())
-                .commit()
+            replaceFragment(RegisterBasicInfoFragment())
         }
+
+        // Next button
+        val nextButton: Button = findViewById(R.id.register_next_button)
+        nextButton.setOnClickListener {
+            var currentFragment = supportFragmentManager.findFragmentById(R.id.register_fragment_container)
+            when (currentFragment) {
+                is RegisterBasicInfoFragment -> {
+                    replaceFragment(RegisterLoginInfoFragment())
+                    replaceHeadline(getString(R.string.register_login_info))
+                }
+
+            }
+        }
+
+        // Back button
+        val backButton: Button = findViewById(R.id.register_back_button)
+        backButton.setOnClickListener {
+            var currentFragment = supportFragmentManager.findFragmentById(R.id.register_fragment_container)
+            when (currentFragment) {
+                is RegisterLoginInfoFragment -> {
+                    replaceFragment(RegisterBasicInfoFragment())
+                    replaceHeadline(getString(R.string.register_basic_info))
+                }
+            }
+        }
+
+
     }
 
     private fun replaceFragment(fragment: Fragment) {
-        val fragmentTransaction = supportFragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.frame_layout, fragment)
-        fragmentTransaction.commit()
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.register_fragment_container, fragment)
+            .commit()
     }
 
-    private fun replaceHeadline(fragment: Fragment): String {
-        return "test"
+    override fun onFragmentInteraction(data: Bundle)  {
+        // Handle the received data from the fragment
+        val name = data.getString("name")
+        // Get other data and store them as needed
+    }
+
+    private fun replaceHeadline(headline: String) {
+        findViewById<TextView>(R.id.register_basic_headline).text = headline
     }
 
 }
--- app/src/main/java/com/gremoryyx/kerjasama/RegisterBasicInfoFragment.kt
+++ app/src/main/java/com/gremoryyx/kerjasama/RegisterBasicInfoFragment.kt
@@ -1,10 +1,16 @@
 package com.gremoryyx.kerjasama
 
+import android.app.DatePickerDialog
+import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.*
+import java.util.*
+
 
 class RegisterBasicInfoFragment : Fragment() {
 
@@ -18,6 +24,95 @@ class RegisterBasicInfoFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val items = listOf("Gender", "Male", "Female")
+        val adapter = CustomArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, items)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        val spinner = view.findViewById<Spinner>(R.id.gender_spinner)
+        spinner.adapter = adapter
+
+        val datePickerButton = view.findViewById<Button>(R.id.date_picker_button)
+        datePickerButton.setOnClickListener {
+            showDatePickerDialog()
+        }
+
     }
 
+    private var listener: OnFragmentInteractionListener? = null
+    private var mListener: RegisterBasicInfoFragment.OnFragmentInteractionListener? = null
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is OnFragmentInteractionListener) {
+            listener = context
+        } else {
+            throw RuntimeException("$context must implement OnFragmentInteractionListener")
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
+    }
+
+    private fun sendDataToActivity() {
+        val data = Bundle()
+        data.putString("name", "John Doe") // Replace with the actual data from the input fields
+        // Add other data as needed
+        listener?.onFragmentInteraction(data)
+    }
+
+
+
+    private fun showDatePickerDialog() {
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+        val onDateSetListener = DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
+            val button: Button = requireView().findViewById(R.id.date_picker_button)
+            button.text = "${dayOfMonth}/${month + 1}/$year"
+        }
+
+        val datePickerDialog = DatePickerDialog(requireContext(), onDateSetListener, year, month, day)
+
+        datePickerDialog.setOnShowListener {
+            datePickerDialog.getButton(DatePickerDialog.BUTTON_NEGATIVE).setTextColor(Color.argb(255, 51, 51, 51))
+            datePickerDialog.getButton(DatePickerDialog.BUTTON_POSITIVE).setTextColor(Color.argb(255, 237, 45, 45))
+        }
+
+        datePickerDialog.show()
+    }
+
+
+
+    // Pass to activity interface
+    interface OnFragmentInteractionListener {
+        fun onFragmentInteraction(data: Bundle)
+    }
+
+}
+
+class CustomArrayAdapter(context: Context, resource: Int, objects: List<String>) :
+    ArrayAdapter<String>(context, resource, objects) {
+
+    override fun isEnabled(position: Int): Boolean {
+        // Disable the first item, make other items selectable
+        return position != 0
+    }
+
+    override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val view = super.getDropDownView(position, convertView, parent)
+
+        // Change the appearance of the first item to indicate it's not selectable
+        val textView = view as TextView
+        if (position == 0) {
+            textView.setTextColor(Color.GRAY)
+        } else {
+            textView.setTextColor(Color.argb(255, 51, 51, 51))
+        }
+        return view
+    }
 }
