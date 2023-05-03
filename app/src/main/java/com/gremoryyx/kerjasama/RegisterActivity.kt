80714bb6f19dc33d74521209244135722a35d329
Binkozaru <binmy2000@gmail.com>
2023-05-03 23:38:44 +0800

    Register new user DONE: - create new user in authentication of firebase - after created, store the user information to the firestore's User collection and create the documentation (document id == user!!.uid)
    Notification COMPLETED:
    Notification message will shown based on the user value's [user references]

--- app/src/main/java/com/gremoryyx/kerjasama/RegisterActivity.kt
+++ app/src/main/java/com/gremoryyx/kerjasama/RegisterActivity.kt
@@ -4,6 +4,7 @@ import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -11,6 +12,12 @@ import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.auth.User
+import com.google.firebase.ktx.Firebase
 
 class RegisterActivity : AppCompatActivity(),
     RegisterLoginInfoFragment.OnLoginInfoFragmentInteractionListener,
@@ -20,15 +27,18 @@ class RegisterActivity : AppCompatActivity(),
 
     // Stored user input data
     private val userData = Bundle()
+    private lateinit var auth: FirebaseAuth
+    private lateinit var db: FirebaseFirestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
         if (savedInstanceState == null) {
-            replaceFragment(RegisterTermsAndConditionsFragment())
+            replaceFragment(RegisterBasicInfoFragment())
         }
 
+        db = FirebaseFirestore.getInstance()
         // Next button
         val nextButton: Button = findViewById(R.id.register_next_button)
         nextButton.setOnClickListener {
@@ -69,6 +79,8 @@ class RegisterActivity : AppCompatActivity(),
                     replaceHeadline(getString(R.string.register_terms_and_conditions_headline))
                 }
                 is RegisterTermsAndConditionsFragment -> {
+                    // Step 5
+                    registerNewUsers(userData.getString("email")!!, userData.getString("password")!!)
                     showCompleteDialog()
                 }
             }
@@ -138,6 +150,44 @@ class RegisterActivity : AppCompatActivity(),
     private fun replaceHeadline(headline: String) {
         findViewById<TextView>(R.id.register_basic_headline).text = headline
     }
+
+    private fun registerNewUsers(email:String, password:String){
+        auth = Firebase.auth
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("Register msg", "createUserWithEmail:success")
+                    task.result?.user?.let {
+                        val newUser = UserData()
+                        newUser.name = userData.getString("name")!!
+                        newUser.username = userData.getString("username")!!
+                        newUser.gender = userData.getString("gender")!!
+                        newUser.date_of_birth = userData.getString("date_of_birth")!!
+                        newUser.ic_number = userData.getString("ic_number")!!
+                        newUser.phone_number = "0"+userData.getString("phone_number")!!
+                        newUser.highest_qualifications = userData.getString("highest_qualifications")!!
+                        db.collection("User").document("${it.uid}").set(newUser)
+                            .addOnSuccessListener {
+                                Log.d("User", "User added to database")
+                            }
+                            .addOnFailureListener {
+                                Log.d("User", "Failed to add user to database")
+                            }
+                    }
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("Register msg", "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        baseContext,
+                        "Authentication failed.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                }
+            }
+    }
+
+
 
     override fun onLoginInfoFragmentInteraction(data: Bundle) {
         // This method is not used anymore, but we still need to implement it to satisfy the interface requirements
---
+++ app/src/main/java/com/gremoryyx/kerjasama/UserData.kt
@@ -0,0 +1,17 @@
+package com.gremoryyx.kerjasama
+
+import android.graphics.Bitmap
+
+data class UserData(
+    var name: String,
+    var username:String,
+    var ic_number: String,
+    var gender: String,
+    var date_of_birth: String,
+    var phone_number: String,
+    var highest_qualifications: String,
+
+
+){
+    constructor(): this("", "", "", "", "", "", "")
+}
