de26d68e29c0e30006cbfbf770b6c95f48b62d0a
gremoryyx <arispelgremory@gmail.com>
2023-05-10 09:37:56 +0800

    savable liao

--- app/src/main/java/com/gremoryyx/kerjasama/RegisterSetupProfilePictureFragment.kt
+++ app/src/main/java/com/gremoryyx/kerjasama/RegisterSetupProfilePictureFragment.kt
@@ -2,6 +2,9 @@ package com.gremoryyx.kerjasama
 
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.drawable.BitmapDrawable
 import android.media.Image
 import android.os.Bundle
 import android.provider.MediaStore
@@ -15,12 +18,15 @@ import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import java.io.File
+import java.io.FileNotFoundException
+import java.io.FileOutputStream
+import java.io.OutputStream
 
 class RegisterSetupProfilePictureFragment : Fragment() {
 
     private lateinit var username: String
     private var ProfilePictureListener: RegisterSetupProfilePictureFragment.OnProfilePictureFragmentInteractionListener? = null
-    private val GALLERY_REQUEST_CODE = 100
     private val getPhoto = registerForActivityResult(ActivityResultContracts.GetContent()){ uri ->
         if(uri != null){
             requireView().findViewById<ImageView>(R.id.register_profile_picture).setImageURI(uri)
@@ -40,6 +46,12 @@ class RegisterSetupProfilePictureFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         getView()?.findViewById<TextView>(R.id.register_username_profile_label)?.text = username
 
+        // Save profile picture on click listener
+        val image = readProfilePicture()
+        if(image != null){
+            getView()?.findViewById<ImageView>(R.id.register_profile_picture)?.setImageBitmap(image)
+        }
+
         // Image on click listener to open the gallery
         getView()?.findViewById<ImageView>(R.id.register_profile_picture)?.setOnClickListener {
             getPhoto.launch("image/*")
@@ -53,6 +65,44 @@ class RegisterSetupProfilePictureFragment : Fragment() {
 
     public fun setUsername(text: String) {
         username = text
+    }
+
+    private fun saveProfilePicture(view: View) {
+        val filename = "profile.png"
+        val file = File(this.context?.filesDir, filename)
+        val image = view as ImageView
+
+        val bd = image.drawable as BitmapDrawable
+        val bitmap = bd.bitmap
+        val outputStream: OutputStream
+
+        try{
+            outputStream = FileOutputStream(file)
+            bitmap.compress(Bitmap.CompressFormat.PNG, 50, outputStream)
+            outputStream.flush()
+            outputStream.close()
+
+            // Add firebase code here
+
+
+        } catch (e: FileNotFoundException){
+            Toast.makeText(requireContext(), "Image not found!", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun readProfilePicture(): Bitmap? {
+        val filename = "profile.png"
+        val file = File(this.context?.filesDir, filename)
+
+        if(file.isFile){
+            try{
+                val bitmap = BitmapFactory.decodeFile(file.absolutePath)
+                return bitmap
+            }catch (e: FileNotFoundException){
+                e.printStackTrace()
+            }
+        }
+        return null
     }
 
 }
